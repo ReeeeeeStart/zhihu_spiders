de36836f6b1a92b53d9ebe392d5f6c66ed9ff41f
ReeeeeeStart <1416906645@qq.com>
2018-02-14 18:06:41 +0800

    ItemPipeline模块的item文件
    在item.py文件中定义所需要的字段

---
+++ items.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+import os
+
+
+class Python123DemoItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    question_title = scrapy.Field()
+    question_detail = scrapy.Field()
+    question_url = scrapy.Field()
+    answer_author = scrapy.Field()
+    answer_voters = scrapy.Field()
+    answer_detail = scrapy.Field()
+
